f5828617a2c22d1117a93765d41215bf1659c3a8
bbogeul <illumeweb@gmail.com>
2020-04-02 19:15:21 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT; // process.env
+
+// error handler import
+const handle = require('./handlers/index');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'hello world' });
+});
+
+// not found error
+app.use(handle.notFound);
+
+// error handler
+app.use(handle.errorHandler);
+
+app.listen(
+  port,
+  console.log(`Server working at ${port} at ${process.env.NODE_ENV}`),
+);
